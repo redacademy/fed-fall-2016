01053c68b2cc6251a77ccee39812003b39751b47
Tracey <traceywindsor@gmail.com>
2016-11-28 12:15:44 -0800

    add ex-navigation and initial route: About

---
+++ app/components/About/styles.js
@@ -0,0 +1,22 @@
+import {
+    StyleSheet,
+} from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        padding: 18,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        fontWeight: 'bold',
+        fontSize: 24,
+    },
+    subheader: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    font: {
+        fontSize: 18,
+    }
+})
---
+++ app/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+  NavigationProvider,
+  StackNavigation,
+} from '@exponent/ex-navigation';
+
+import { Router } from './navigation/routes.js'
+
+
+/*
+default navigation: vertical
+*/
+export default class ChangedAndFed extends Component {
+  render() {
+    return (
+      <NavigationProvider router={Router}>
+        <StackNavigation initialRoute={Router.getRoute('navigationLayout')} id="mainStack" />
+      </NavigationProvider>
+    );
+  }
+}
