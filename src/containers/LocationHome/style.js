675ddec1ca8e18ffe85545e8cfde9dd9159dba20
brandonfajardo <brandonjfajardo@hotmail.com>
2016-11-29 12:32:17 -0800

    Add SearchBar into LocationHome, reposition

--- src/containers/LocationHome/style.js
+++ src/containers/LocationHome/style.js
@@ -1,7 +1,14 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
+      flexDirection: 'column'
+    },
+    searchContainer: {
+      padding: 15,
+      position: 'absolute',
+      bottom: 0,
+      width: Dimensions.get('window').width
     }
 })
