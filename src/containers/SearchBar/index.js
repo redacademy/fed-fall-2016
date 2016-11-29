3e2e36d92e4aa0dbea5d332bba4a6341cc188779
brandonfajardo <brandonjfajardo@hotmail.com>
2016-11-28 17:36:37 -0800

    Clean up code

--- src/containers/SearchBar/index.js
+++ src/containers/SearchBar/index.js
@@ -3,15 +3,25 @@ import { TextInput } from 'react-native'
 import { styles } from './style'
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            text: ''
+        }
+
+    }
+    
     render(){
         return (
             <TextInput 
+                onChangeText={(text) => this.setState({ text })}
+                value={this.state.text}
                 placeholder="Search"
                 autoCorrect={false}
-                label="O"
-                style={styles.TextInput}/>
+                style={styles.TextInput} />
         )
     }
 }
 
-export default SearchBar;
+export default SearchBar
--- src/containers/SearchBar/style.js
+++ src/containers/SearchBar/style.js
@@ -3,13 +3,13 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     TextInput: {
         height: 40,
-        width: 270,
+        width: 260,
         borderColor: 'lightgray',
         borderWidth: 1,
         borderRadius: 20,
         paddingTop: 10, 
         paddingBottom: 10,
-        paddingLeft: 15,
-        paddingRight: 15,
+        paddingLeft: 20,
+        paddingRight: 20,
     }
 })
