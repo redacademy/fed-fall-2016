202e3e0585399d43eaf16ea32bf5db2cfdff4a54
fergmac <f.macconnell@gmail.com>
2016-11-29 15:08:43 -0800

    update optionsBarButton styles

--- src/components/LocationHomeOptionsBar/styles.js
+++ src/components/LocationHomeOptionsBar/styles.js
@@ -11,15 +11,17 @@ export const styles = StyleSheet.create({
     button: {
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'white',
+        backgroundColor: 'rgb(253, 253, 253)',
         height: 60,
         width: 60,
         borderRadius: 30,
-        borderColor: 'lightgrey',
-        borderWidth: 1,
+        shadowColor: 'lightgrey',
+        shadowOpacity: 0.6,
+        shadowOffset: { width: 1, height: -2, },
+        shadowRadius: 3,
     },
     icon: {
         color: 'grey',
-        fontSize: 30
+        fontSize: 30,
     },
 })
