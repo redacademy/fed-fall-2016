9c0f932963eb5c41a5ad9e24082bca2effd7a995
fergmac <f.macconnell@gmail.com>
2016-11-30 14:51:15 -0800

    adjust LocationHome button styles #17 #26

--- src/components/LocationHomeBottomButton/styles.js
+++ src/components/LocationHomeBottomButton/styles.js
@@ -16,12 +16,15 @@ export const styles = StyleSheet.create({
         height: 60,
         width: 60,
         borderRadius: 30,
-        borderColor: 'lightgrey',
-        borderWidth: 1,
+        shadowColor: 'lightgrey',
+        shadowOpacity: 0.6,
+        shadowOffset: { width: 1, height: -2, },
+        shadowRadius: 3,
         marginLeft: 10,
     },
     icon: {
         color: 'grey',
         fontSize: 30,
+
     },
 })
