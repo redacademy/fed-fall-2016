6193d8a8cb42917c224fdced54418566240257ea
scottclayton <i@scottc.ca>
2016-11-30 09:56:04 -0800

    add app-wide stylesheet for hexColours #25

--- src/config/styles.js
+++ src/config/styles.js
@@ -1,19 +1,37 @@
-import { StyleSheet } from 'react-native'
+// import { StyleSheet } from 'react-native'
 
-export const rgbColors = {
+export const rgbColours = {
   white: 'rgb(255, 255, 255)',
-  warmGrey: 'rgb(127, 127, 127)',
-  darkGreyBlue: 'rgb(45, 76, 96)',
-  blush: 'rgb(241, 121, 121)',
   whiteTwo: 'rgb(253, 253, 253)',
-  warmGreyTwo: 'rgb(150, 150, 150)',
-  warmGreyThree: 'rgb(127, 126, 126)',
-  lightMauve: 'rgb(197, 151, 189)',
   greyish: 'rgb(172, 172, 172)',
+  warmGrey: 'rgb(127, 127, 127)',
+  warmGreyTwo: 'rgb(150, 150, 150)',
+  darkGreyBlue: 'rgb(45, 76, 96)',
+  warmGreyThree: 'rgb(127, 126, 126)',
   beige: 'rgb(248, 251, 244)',
+  lightMauve: 'rgb(197, 151, 189)',
   lightGreyGreen: 'rgb(191, 216, 153)',
-  apricot: 'rgb(255, 198, 116)',
   lightGreyGreenTwo: 'rgb(203, 230, 162)',
+  apricot: 'rgb(255, 198, 116)',
+  blush: 'rgb(241, 121, 121)',
   salmon: 'rgb(255, 139, 108)',
   darkPeach: 'rgb(231, 116, 116)',
+}
+
+export const hexColours = {
+  white: '#ffffff',
+  whiteTwo: '#fdfdfd',
+  greyish: '#acacac',
+  warmGrey: '#7f7f7f',
+  warmGreyTwo: '#969696',
+  darkGreyBlue: '#2d4c60',
+  warmGreyThree: '#7f7e7e',
+  beige: '#f8fbf4',
+  lightMauve: '#c597bd',
+  lightGreyGreen: '#bfd899',
+  lightGreyGreenTwo: '#cbe6a2',
+  apricot: '#ffc674',
+  blush: '#f17979',
+  salmon: '#ff8b6c',
+  darkPeach: '#e77474',
 }
