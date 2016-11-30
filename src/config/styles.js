1b26627d8730352b68ce869924fb836ac2067bef
scottclayton <i@scottc.ca>
2016-11-30 09:39:47 -0800

    add app-wide stylesheet for rbgColours #25

---
+++ src/config/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+export const rgbColors = {
+  white: 'rgb(255, 255, 255)',
+  warmGrey: 'rgb(127, 127, 127)',
+  darkGreyBlue: 'rgb(45, 76, 96)',
+  blush: 'rgb(241, 121, 121)',
+  whiteTwo: 'rgb(253, 253, 253)',
+  warmGreyTwo: 'rgb(150, 150, 150)',
+  warmGreyThree: 'rgb(127, 126, 126)',
+  lightMauve: 'rgb(197, 151, 189)',
+  greyish: 'rgb(172, 172, 172)',
+  beige: 'rgb(248, 251, 244)',
+  lightGreyGreen: 'rgb(191, 216, 153)',
+  apricot: 'rgb(255, 198, 116)',
+  lightGreyGreenTwo: 'rgb(203, 230, 162)',
+  salmon: 'rgb(255, 139, 108)',
+  darkPeach: 'rgb(231, 116, 116)',
+}
