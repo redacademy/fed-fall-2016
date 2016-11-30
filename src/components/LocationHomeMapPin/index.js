04ff24586a403050ccfedc6b92b996013d6b9627
Tracey <traceywindsor@gmail.com>
2016-11-30 14:28:31 -0800

    merge LocationHomeOptionsBar

---
+++ src/components/LocationHomeMapPin/index.js
@@ -0,0 +1,26 @@
+import React, { Component, PropTypes } from 'react'
+import {
+    View,
+    TouchableOpacity,
+    Text,
+} from 'react-native'
+import { styles } from './styles'
+
+class LocationHomeMapPin extends Component {
+    static propTypes = {
+
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.box} onPress={this.props.onPress}>
+                    <View style={styles.button}>
+                        {/* <Icon style={styles.icon} name="filter-list" size={60} color="lightgrey" /> */}
+                    </View>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+export default LocationHomeMapPin
