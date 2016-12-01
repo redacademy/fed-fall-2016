04c47234b23b39e230fc5e3dc2b6957266c2b156
Tracey <traceywindsor@gmail.com>
2016-11-30 20:28:00 -0800

    update mockdata

--- src/assets/mockdata.js
+++ src/assets/mockdata.js
@@ -1,8 +1,20 @@
-import {LOW, MEDIUM, HIGH} from './constants'
+import { LOW, MEDIUM, HIGH } from './constants'
 
+/*The mock data should include:
+locations: [{
+    id,
+    title,
+    coordinate{
+        latitude,
+        longitude,
+    },
+    reviews,
+    image urls,
+}]
+*/
 export const mockLocations = [{
     id: 0,
-    name: "Boston Pizza",
+    title: "Boston Pizza",
     place_id: "ChIJW2wi48ZzhlQRMrotX09z9xw",
     coordinate: {
         latitude: 49.26363504526748,
@@ -11,22 +23,25 @@ export const mockLocations = [{
     amenities: {
         changeTable: true,
         nursingRoom: false,
-        washroomGender: 'women',
+        washroomGender: ["women"],
         familyWashroom: false,
         privacy: true,
         strollerAccessible: true,
-        quiet: true, 
+        quiet: true,
         requiresKey: false,
     },
-    rating: {
+    ratings: {
         quality: MEDIUM,
         clean: MEDIUM,
         nursing: LOW,
         quiet: LOW,
     },
+    photos: {
+        url: "https://s-media-cache-ak0.pinimg.com/236x/25/20/f8/2520f8b0b1ccae6253e2975c766c334a.jpg"
+    },
 }, {
     id: 1,
-    name: "Denny's",
+    title: "Dennys",
     place_id: "ChIJ2xyFx-FzhlQR7AmrwU4HwFk",
     coordinate: {
         latitude: 49.26327075476226,
@@ -35,22 +50,25 @@ export const mockLocations = [{
     amenities: {
         changeTable: true,
         nursingRoom: false,
-        washroomGender: 'women',
+        washroomGender: ["women", "men"],
         familyWashroom: false,
         privacy: true,
         strollerAccessible: false,
-        quiet: false, 
+        quiet: false,
         requiresKey: false,
     },
-    rating: {
+    ratings: {
         quality: MEDIUM,
         clean: MEDIUM,
         nursing: LOW,
         quiet: MEDIUM,
     },
+    photos: {
+        url: "https://s-media-cache-ak0.pinimg.com/236x/25/20/f8/2520f8b0b1ccae6253e2975c766c334a.jpg"
+    },
 }, {
     id: 2,
-    name: "McDonald's",
+    title: "McDonalds",
     place_id: "ChIJbW0YnsdzhlQR2mtc9pcWMWw",
     coordinate: {
         latitude: 49.26340736541866,
@@ -59,22 +77,25 @@ export const mockLocations = [{
     amenities: {
         changeTable: true,
         nursingRoom: false,
-        washroomGender: 'women',
+        washroomGender: ["women", "men"],
         familyWashroom: false,
         privacy: true,
         strollerAccessible: false,
-        quiet: false, 
+        quiet: false,
         requiresKey: false,
     },
-    rating: {
+    ratings: {
         quality: LOW,
         clean: LOW,
         nursing: LOW,
         quiet: LOW,
     },
+    photos: {
+        url: "https://pbs.twimg.com/profile_images/605432803560267776/Iun--DFs.jpg"
+    },
 }, {
     id: 3,
-    name: "Blenz",
+    title: "Blenz",
     place_id: "ChIJP2FxnsdzhlQRtGPPIr583EQ",
     coordinate: {
         latitude: 49.26341516675726,
@@ -83,22 +104,25 @@ export const mockLocations = [{
     amenities: {
         changeTable: true,
         nursingRoom: false,
-        washroomGender: 'women',
+        washroomGender: ["women"],
         familyWashroom: false,
         privacy: true,
         strollerAccessible: true,
-        quiet: false, 
+        quiet: false,
         requiresKey: true,
     },
-    rating: {
+    ratings: {
         quality: MEDIUM,
         clean: MEDIUM,
         nursing: LOW,
         quiet: MEDIUM,
     },
+    photos: {
+        url: "https://s-media-cache-ak0.pinimg.com/236x/25/20/f8/2520f8b0b1ccae6253e2975c766c334a.jpg"
+    },
 }, {
     id: 4,
-    name: "Cactus Club",
+    title: "Cactus Club",
     place_id: "ChIJx_AshMdzhlQRNYTKRxzKHGE",
     coordinate: {
         latitude: 49.26336347313966,
@@ -106,23 +130,26 @@ export const mockLocations = [{
     },
     amenities: {
         changeTable: true,
-        nursingRoom: false,
-        washroomGender: 'women',
+        nursingRoom: true,
+        washroomGender: ["women"],
         familyWashroom: false,
         privacy: true,
         strollerAccessible: true,
-        quiet: false, 
+        quiet: false,
         requiresKey: false,
     },
-    rating: {
+    ratings: {
         quality: HIGH,
         clean: HIGH,
         nursing: MEDIUM,
         quiet: MEDIUM,
     },
+    photos: {
+        url: "https://redtricom.files.wordpress.com/2012/11/2011-03-louis-vuitton-dubai-mall-of-the-emirates-family-room-childrens.jpg"
+    },
 }, {
     id: 5,
-    name: "Masa Japanese Restaurant",
+    title: "Masa Japanese Restaurant",
     place_id: "ChIJ5Qg8CcdzhlQRLVzT-a95-FA",
     coordinate: {
         latitude: 49.26338133296119,
@@ -131,22 +158,25 @@ export const mockLocations = [{
     amenities: {
         changeTable: true,
         nursingRoom: false,
-        washroomGender: 'women',
+        washroomGender: ["women"],
         familyWashroom: false,
         privacy: true,
         strollerAccessible: false,
-        quiet: false, 
+        quiet: false,
         requiresKey: false,
     },
-    rating: {
+    ratings: {
         quality: LOW,
         clean: MEDIUM,
         nursing: LOW,
         quiet: MEDIUM,
     },
+    photos: {
+        url: "https://pbs.twimg.com/profile_images/605432803560267776/Iun--DFs.jpg"
+    },
 }, {
     id: 6,
-    name: "Vancouver Public Library, Firehall Branch",
+    title: "Vancouver Public Library, Firehall Branch",
     place_id: "ChIJh_Vsb8dzhlQRo3zWwxVDwbI",
     coordinate: {
         latitude: 49.26283545734029,
@@ -155,22 +185,25 @@ export const mockLocations = [{
     amenities: {
         changeTable: true,
         nursingRoom: true,
-        washroomGender: 'women',
+        washroomGender: ["women", "men"],
         familyWashroom: false,
         privacy: true,
         strollerAccessible: true,
-        quiet: false, 
+        quiet: false,
         requiresKey: true,
     },
-    rating: {
+    ratings: {
         quality: LOW,
         clean: MEDIUM,
         nursing: MEDIUM,
         quiet: HIGH,
     },
+    photos: {
+        url: "https://s-media-cache-ak0.pinimg.com/736x/c4/53/3b/c4533bea11c289d895be6caa320d4328.jpg"
+    },
 }, {
     id: 7,
-    name: "JOEY Broadway",
+    title: "JOEY Broadway",
     place_id: "ChIJFfDeDsdzhlQRqU3zGHTmMuk",
     coordinate: {
         latitude: 49.26323518987539,
@@ -179,22 +212,25 @@ export const mockLocations = [{
     amenities: {
         changeTable: true,
         nursingRoom: true,
-        washroomGender: 'women',
+        washroomGender: ["women", "men"],
         familyWashroom: true,
         privacy: true,
         strollerAccessible: true,
-        quiet: false, 
+        quiet: false,
         requiresKey: false,
     },
-    rating: {
+    ratings: {
         quality: MEDIUM,
         clean: HIGH,
         nursing: LOW,
         quiet: LOW,
     },
+    photos: {
+        url: "https://s-media-cache-ak0.pinimg.com/236x/25/20/f8/2520f8b0b1ccae6253e2975c766c334a.jpg"
+    },
 }, {
     id: 8,
-    name: "Starbucks",
+    title: "Starbucks",
     place_id: "ChIJVTbKnMdzhlQR2eJyHFphPec",
     coordinate: {
         latitude: 49.26311999384853,
@@ -203,22 +239,25 @@ export const mockLocations = [{
     amenities: {
         changeTable: true,
         nursingRoom: false,
-        washroomGender: 'women',
+        washroomGender: ["women"],
         familyWashroom: false,
         privacy: true,
         strollerAccessible: true,
-        quiet: false, 
+        quiet: false,
         requiresKey: false,
     },
-    rating: {
+    ratings: {
         quality: MEDIUM,
         clean: MEDIUM,
         nursing: LOW,
         quiet: LOW,
     },
+    photos: {
+        url: "https://s-media-cache-ak0.pinimg.com/236x/25/20/f8/2520f8b0b1ccae6253e2975c766c334a.jpg"
+    },
 }, {
     id: 9,
-    name: "Paul's Omelettery",
+    title: "Pauls Omelettery",
     place_id: "ChIJk5Qv1cdzhlQRC9m9Hes_il8",
     coordinate: {
         latitude: 49.26592322194549,
@@ -227,17 +266,89 @@ export const mockLocations = [{
     amenities: {
         changeTable: true,
         nursingRoom: false,
-        washroomGender: 'women',
+        washroomGender: ["women"],
         familyWashroom: false,
         privacy: true,
         strollerAccessible: false,
-        quiet: false, 
+        quiet: false,
         requiresKey: false,
     },
-    rating: {
+    ratings: {
         quality: LOW,
         clean: MEDIUM,
         nursing: LOW,
         quiet: LOW,
     },
+    photos: {
+        url: "https://pbs.twimg.com/media/CTcF2ogW4AAIfoF.png"
+    },
+}]
+/*
+users: [{
+    id,
+    username,
+    email,
+    password,
+}]
+*/
+export const mockUsers = [{
+    id: 0,
+    name: "Bonnie Francis",
+    email: "bonnie@mail.com",
+    password: "Francis1",
+},{
+    id: 1,
+    name: "Bob Frank",
+    email: "frank@mail.com",
+    password: "Frank1",
+},{
+    id: 2,
+    name: "Fanny May",
+    email: "fanny@mail.com",
+    password: "Fanny1",
+},{
+    id: 3,
+    name: "Fred Man",
+    email: "fred@mail.com",
+    password: "FredMan1",
+},{
+    id: 4,
+    name: "Sarah Cooper",
+    email: "sarah@mail.com",
+    password: "Sarah1",
+},{
+    id: 5,
+    name: "Stephen Chen",
+    email: "stephen@mail.com",
+    password: "Stephen1",
+},{
+    id: 6,
+    name: "Tamara Smith",
+    email: "tamara@mail.com",
+    password: "Tamara1",
+},{
+    id: 7,
+    name: "Tim Sutherland",
+    email: "tim@mail.com",
+    password: "Sutherland1",
+},{
+    id: 8,
+    name: "Marie Kwon",
+    email: "marie@mail.com",
+    password: "Marie1",
+},{
+    id: 9,
+    name: "Mohammad Khan",
+    email: "mohammad@mail.com",
+    password: "Mohammad1",
+},{
+    id: 10,
+    name: "Suzy Major",
+    email: "suzy@mail.com",
+    password: "Major1",
+},{
+    id: 11,
+    name: "Seth Minor",
+    email: "seth@mail.com",
+    password: "Seth",
 }]
