25bfc1f79a447e227b7b81b0156f1c7bf34033a9
fergmac <f.macconnell@gmail.com>
2016-12-01 14:57:23 -0800

    build ListViewItem #44

--- src/components/ListViewItem/index.js
+++ src/components/ListViewItem/index.js
@@ -4,7 +4,7 @@ import {
     ListView,
     Text,
 } from 'react-native'
-// import { styles } from './styles'
+import { styles } from './style'
 
 
 
@@ -13,8 +13,13 @@ class ListViewItem extends Component {
     render() {
         console.log("data", this.props.locations)
         return (
-            <View>
-                <View><Text>{this.props.locations.title}</Text></View>
+            <View style={styles.locationContainer}>
+                <View style={styles.map}><Text>MAP</Text></View>
+                <View style={styles.locationDetails}>
+                    <Text>{this.props.locations.title}</Text>
+                    <Text>{this.props.locations.place_id}</Text>
+                    <Text>100 Meters</Text>
+                </View>
             </View>
         )
     }
--- src/components/ListViewItem/style.js
+++ src/components/ListViewItem/style.js
@@ -3,7 +3,21 @@ import {
 } from 'react-native';
 
 export const styles = StyleSheet.create({
-    container: {
+    locationContainer: {
         flex: 1,
+        flexDirection: 'row',
+        height: 140,
+    },
+    map:{
+        height: 120,
+        width: 120,
+        borderWidth: 2,
+        margin: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    locationDetails: {
+        paddingRight: 2,
+        margin: 10
     }
 })
