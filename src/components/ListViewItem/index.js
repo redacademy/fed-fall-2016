05f285bb44f2f8c8958468803141a425a469e05c
fergmac <f.macconnell@gmail.com>
2016-12-03 14:17:19 -0800

    adjust google maps api call results

--- src/components/ListViewItem/index.js
+++ src/components/ListViewItem/index.js
@@ -12,7 +12,7 @@ class ListViewItem extends Component {
     constructor() {
         super()
         this.state = {
-            location: {},
+            location: [],
         }
     }
 
@@ -25,7 +25,7 @@ class ListViewItem extends Component {
             method: 'GET',
         }).then(response => response.json())
             .then((results) => {
-                this.setState({ location: results })
+                this.setState({ location: results }) //&loading false
             })
     }
     componentDidMount() {
@@ -34,12 +34,14 @@ class ListViewItem extends Component {
 
 
     render() {
+        console.log(this.state)
+        console.log("results", this.state.location.results)
         // const lat = this.props.placeId.coordinate.latitude;
         // const long = this.props.placeId.coordinate.longitude;
-        const lat = this.state.location.results[0].geometry.location.lat
-        const lng = this.state.location.results[0].geometry.location.lng
+        const lat = this.state.location[0].geometry.location.lat
+        const lng = this.state.location[0].geometry.location.lng
         // const places = `https://maps.googleapis.com/maps/api/geocode/json?place_id=${place_id}&key=AIzaSyB2WkbsqNDjsiz8i831IVn1piVIq5OeiCI`
-        console.log("address", this.state.location.results[0].formatted_address)
+        console.log("address", this.state.location[0].formatted_address)
         return (
             <TouchableOpacity onPress={() => { } } >
                 <View style={styles.locationContainer}>
@@ -47,10 +49,10 @@ class ListViewItem extends Component {
                         style={styles.map}
                         source={{ url: `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=18&size=120x120&maptype=roadmap&label:%20&key=AIzaSyB2WkbsqNDjsiz8i831IVn1piVIq5OeiCI` }}
                         />
-                    <Text>{this.state.location.results[0].address_components[0].long_name}</Text>
+                    <Text>{this.state.location[0].address_components[0].long_name}</Text>
                     <Text>
-                        {this.state.location.results[0].address_components[1].long_name}
-                        {this.state.location.results[0].address_components[2].long_name}
+                        {this.state.location[0].address_components[1].long_name}
+                        {this.state.location[0].address_components[2].long_name}
                     </Text>
                 </View>
             </TouchableOpacity>
