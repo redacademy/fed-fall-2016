f410c357d7a5166b00ee45953d62bfafb55211db
Tracey <traceywindsor@gmail.com>
2016-12-05 10:07:15 -0800

    Update Card to require height and takes Children

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,40 @@
+import React, { Component, PropTypes, } from 'react'
+import {
+    View,
+} from 'react-native'
+import { styles } from './style'
+
+
+class Card extends Component {
+    static propTypes = {
+        height: PropTypes.number.isRequired,
+    }
+
+    render() {
+        return (
+        <View style={[styles.CardContainer, {height: this.props.height, }]}>
+                {this.props.children}
+        </View>
+        )
+    }
+}
+
+export default Card
+
+
+
+/*
 import React from 'react'
 import { View, Text } from 'react-native'
 import { styles } from './style'
 
-const Card = ({ children, height}) => (
+const Card = ({height}) => (
         <View style={[styles.CardContainer, {height: height }]}>
             <View style={{overflow: 'hidden'}} >
-                {children}
+                {this.props.children}
             </View>
         </View>
 )
 
 export default Card
+*/
