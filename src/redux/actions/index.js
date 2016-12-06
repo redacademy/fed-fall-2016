cd494e0f5d241c754cbfe2f632f756f7e59242ef
Tracey <traceywindsor@gmail.com>
2016-12-05 20:59:28 -0800

    add actions and reducersfor LocationAdd #19

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,12 +2,14 @@
 export const ON_SEARCH_CHANGE = 'ON_SEARCH_CHANGE'
 export const ENTER_PREVIEW = 'ENTER_PREVIEW'
 export const EXIT_PREVIEW = 'EXIT_PREVIEW'
+export const ENTER_LOCATION_ADD = 'ENTER_LOCATION_ADD'
+export const EXIT_LOCATION_ADD = 'EXIT_LOCATION_ADD'
 
 // Action creators here
 export const searchTextChange = (text) => {
     return {
         type: ON_SEARCH_CHANGE,
-        payload: text
+        payload: text,
     }
 }
 
@@ -17,6 +19,14 @@ export const enterPreview = () => ({
 
 export const exitPreview = () => ({
   type: EXIT_PREVIEW,
+})
+
+export const enterLocationAdd = () => ({
+  type: ENTER_LOCATION_ADD,
+})
+
+export const exitLocationAdd= () => ({
+  type: EXIT_LOCATION_ADD,
 })
 
 // Thunks down here
--- src/redux/reducers/button.js
+++ src/redux/reducers/button.js
@@ -1,15 +1,25 @@
-import { ENTER_PREVIEW, EXIT_PREVIEW } from '../actions'
+import { 
+    ENTER_PREVIEW, 
+    EXIT_PREVIEW,
+    ENTER_LOCATION_ADD, 
+    EXIT_LOCATION_ADD
+} from '../actions'
 
 const initialState = {
-    preview: false
+    preview: false,
+    locationAdd: false,
 }
 
 export default (state = initialState, action) => {
     switch(action.type){
         case ENTER_PREVIEW:
-            return {...state, preview: true}
+            return {...state, preview: true,}
         case EXIT_PREVIEW: 
-            return {...state, preview: false}
+            return {...state, preview: false,}
+        case ENTER_LOCATION_ADD:
+            return {...state, locationAdd: true,}
+        case EXIT_LOCATION_ADD: 
+            return {...state, locationAdd: false,}
         default: 
             return state
     }
