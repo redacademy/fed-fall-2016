a5ed9c0e36a96f09ce6754ab66dd213ab563464b
fergmac <f.macconnell@gmail.com>
2016-12-06 10:38:54 -0800

    create Map reducer

---
+++ src/redux/reducers/map.js
@@ -0,0 +1,15 @@
+import { GOT_ALL_LOCATION_DATA } from '../actions'
+
+const initialState = {
+    generatedLocationData: ''
+}
+
+export default (state = initialState, action) => {
+    switch(action.type){
+        case GOT_ALL_LOCATION_DATA: 
+            return { generatedLocationData: action.payload }
+        default:
+            return state
+    }
+}
+
