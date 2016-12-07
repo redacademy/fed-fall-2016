7d24e4c20ecea3807369bf37fe93f17c9ac5e577
scottclayton <i@scottc.ca>
2016-12-07 10:34:05 -0800

    add notes package to storybook #13

--- storybook/addons.js
+++ storybook/addons.js
@@ -2,3 +2,7 @@ import '@kadira/react-native-storybook/addons'
 
 // knobs
 import '@kadira/storybook-addon-knobs/register'
+
+// notes
+// import '@kadira/storybook/addons'
+import '@kadira/storybook-addon-notes/register'
