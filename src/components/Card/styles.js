0520e2b7916443102c311dd94822a210c5592a8c
brandonfajardo <brandonjfajardo@hotmail.com>
2016-12-07 12:12:45 -0800

    fix conflicts

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -17,7 +17,12 @@ export const styles = StyleSheet.create({
         width: width * 0.945,
         borderRadius: 12,
         flexDirection: 'column'
-    }
+    },
+    CardContent: {
+         flexDirection: 'column',
+         flex: 1,
+         justifyContent: 'flex-end',
+      }
 })
 
 export default styles
