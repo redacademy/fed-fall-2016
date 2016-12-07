1fcd9f948b44dda501802624376714191aedafe3
Tracey <traceywindsor@gmail.com>
2016-12-07 15:19:09 -0800

    code cleanup for #74

--- src/components/MapBlock/index.js
+++ src/components/MapBlock/index.js
@@ -42,38 +42,4 @@ export default MapBlock
 
 //USAGE:
 // import { MapBlock } from '../components/MapBlock'
-//<MapBlock lat={49.2634046} long={-123.1404133} zoom={18} width={250} height={120} pinScale={0.4} pinColor={'red'} iconName={'starbaby-face'} />
-
-/*
-            <Image
-                style={styles.map}
-                source={{ url: `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=18&size=120x120&maptype=roadmap&label:%20&key=AIzaSyB2WkbsqNDjsiz8i831IVn1piVIq5OeiCI` }}
-                >
-                <MapPin scale="0.4"  pinColor={'salmon'} iconName="diaper" />
-                </Image>
-
-
-
-    render() {
-        console.log(this.props)
-        const url = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=${width}x${height}&maptype=roadmap&label:%20&key=AIzaSyB2WkbsqNDjsiz8i831IVn1piVIq5OeiCI`
-
-        console.log('url: ', url)
-        return (
-            <Image
-                style={{
-                    height: this.props.height,
-                    width: this.props.width,
-                    margin: 5,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-                source={{ url: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/event/c/b/5/7/global_439012055.jpeg' }}
-                >
-                <MapPin scale={this.props.pinScale} pinColor={this.props.pinColor} iconName={this.props.iconName} />
-            </Image>
-        )
-    }
-
-    
-*/
+//<MapBlock lat={49.2634046} lng={-123.1404133} zoom={18} width={250} height={120} pinScale={0.4} pinColor={'red'} iconName={'starbaby-face'} />
