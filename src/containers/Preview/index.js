3fbc48be9e602a2248c47dd00263682cfa4fe86f
Tracey <traceywindsor@gmail.com>
2016-12-08 09:44:59 -0800

    fix import

--- src/containers/Preview/index.js
+++ src/containers/Preview/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Dimensions } from 'react'
 import { View, Animated } from 'react-native'
 import { connect } from 'react-redux'
 import {
