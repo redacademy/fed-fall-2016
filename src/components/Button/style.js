10ac3a29f6a2fc1206a977e9df62f418f38e1304
Tracey <traceywindsor@gmail.com>
2016-12-08 14:54:30 -0800

    merge conflicts

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,24 +1,4 @@
 import { StyleSheet, Dimensions } from 'react-native'
-<<<<<<< HEAD
-
-const { width, height } = Dimensions.get('window')
-
-const styles = StyleSheet.create({
-     button: {
-         justifyContent: 'center',
-           alignItems: 'center',
-           height: 45,
-        width: width * 0.80,
-        borderRadius: 30,
-         backgroundColor: '#a3c56d',
-         shadowColor: 'darkgrey',
-         shadowOpacity: 0.5,
-         shadowOffset: { width: 0.2, height: 1, },
-         shadowRadius: 2,
-         alignSelf: 'center',
-     }
- })
-=======
 import { colorPalette } from '../../config/styles'
 
 const { width } = Dimensions.get('window')
@@ -38,5 +18,5 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
     }
 })
->>>>>>> feature-locationadd
+
 export default styles
