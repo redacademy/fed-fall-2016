72e02a1afb0915cc54cab68c58d2a7ba7d13bb19
Tracey <traceywindsor@gmail.com>
2016-12-08 09:47:53 -0800

    merge - redux actions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -6,6 +6,7 @@ export const ENTER_LOCATION_ADD = 'ENTER_LOCATION_ADD'
 export const EXIT_LOCATION_ADD = 'EXIT_LOCATION_ADD'
 export const GOT_ALL_LOCATION_DATA = 'GOT_ALL_LOCATION_DATA'
 export const GOT_LOCATION_DETAILS = 'GOT_LOCATION_DETAILS'
+
 // Action creators here
 export const searchTextChange = (text) => ({
     type: ON_SEARCH_CHANGE,
@@ -13,8 +14,8 @@ export const searchTextChange = (text) => ({
 })
 
 export const enterPreview = (placeid) => ({
-  type: ENTER_PREVIEW,
-  payload: placeid
+    type: ENTER_PREVIEW,
+    payload: placeid,
 })
 
 export const exitPreview = () => ({
@@ -30,29 +31,28 @@ export const exitLocationAdd = () => ({
 })
 
 export const generateMapPins = () => {
-  return function (dispatch) {
-    fetch('http://45.55.2.200/api/location', {
-      method: 'GET',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-      })
-    })
-      .then(response => response.json())
-      .then(mapData => dispatch({ type: 'GOT_ALL_LOCATION_DATA', payload: mapData }))
-  }
+    return function (dispatch) {
+        fetch('http://45.55.2.200/api/location', {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+        })
+            .then(response => response.json())
+            .then(mapData => dispatch({ type: 'GOT_ALL_LOCATION_DATA', payload: mapData }))
+    }
 }
 
 export const getLocationDetails = (placeId) => {
-  return function (dispatch) {
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?place_id=${placeId}&key=AIzaSyB2WkbsqNDjsiz8i831IVn1piVIq5OeiCI`, {
-      method: 'GET',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-      })
-    })
-      .then(response => response.json())
-      .then(locationDetails => dispatch({ type: 'GOT_LOCATION_DETAILS', payload: locationDetails }))
-  }
-
+    return function (dispatch) {
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?place_id=${placeId}&key=AIzaSyB2WkbsqNDjsiz8i831IVn1piVIq5OeiCI`, {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+        })
+            .then(response => response.json())
+            .then(locationDetails => dispatch({ type: 'GOT_LOCATION_DETAILS', payload: locationDetails }))
+    }
 }
 
