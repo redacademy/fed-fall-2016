24405f477439d7c91ea6e74d869c1182dfc79572
Tracey <traceywindsor@gmail.com>
2016-12-08 10:11:37 -0800

    resolve merge conflicts

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-<<<<<<< b3b440e2c74c25919abf41639163bb70aa95eee5
 import { TouchableHighlight } from 'react-native'
 import styles from './style'
 
@@ -8,18 +7,6 @@ const Button = ({ children }) => {
         <TouchableHighlight underlayColor={'#8cb649'} style={styles.button} onPress={() => {}}>
             {children}
         </TouchableHighlight>
-=======
-import { View } from 'react-native'
-import { styles } from './style'
-
-const Button = ({ children }) => {
-    return (
-        <View style={styles.buttonContainer}>
-            <View style={styles.button}>
-                {children}
-            </View>
-        </View>
->>>>>>> resolved conflicts
     )
 }
 
--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,44 +1,20 @@
 import { StyleSheet, Dimensions } from 'react-native'
 
-const { width, height } = Dimensions.get('window')
+const { width } = Dimensions.get('window')
 
-<<<<<<< b3b440e2c74c25919abf41639163bb70aa95eee5
 const styles = StyleSheet.create({
-     button: {
-         justifyContent: 'center',
-           alignItems: 'center',
-           height: 45,
-        width: width * 0.80,
-        borderRadius: 30,
-         backgroundColor: '#a3c56d',
-         shadowColor: 'darkgrey',
-         shadowOpacity: 0.5,
-         shadowOffset: { width: 0.2, height: 1, },
-         shadowRadius: 2,
-         alignSelf: 'center',
-     }
- })
-export default styles
-=======
-export const styles = StyleSheet.create({
-    buttonContainer: {
-        justifyContent: 'center',
-        alignItems: 'flex-end',
-        flexDirection: 'row',
-        flex: 1
-    },
     button: {
+        justifyContent: 'center',
+        alignItems: 'center',
         height: 45,
         width: width * 0.80,
         borderRadius: 30,
         backgroundColor: '#a3c56d',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
         shadowColor: 'darkgrey',
         shadowOpacity: 0.5,
         shadowOffset: { width: 0.2, height: 1, },
         shadowRadius: 2,
+        alignSelf: 'center',
     }
 })
->>>>>>> resolved conflicts
+export default styles
