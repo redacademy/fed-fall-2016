d8c3b0d71c85814efe2e6bfc25c54d1cf4a04762
fergmac <f.macconnell@gmail.com>
2016-12-10 14:16:17 -0800

    nest OptionsBarButton inside LocationHome

---
+++ src/containers/LocationHome/OptionsBarButton/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native'
+import { colors } from '../../../config/styles'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    button: {
+        alignItems: 'center', //icon
+        justifyContent: 'center',
+        backgroundColor: colors.white,
+        height: 50,
+        width: 50,
+        borderRadius: 25,
+        shadowColor: colors.warmGrey,
+        shadowOpacity: 0.6,
+        shadowOffset: { width: 1, height: -2 },
+        shadowRadius: 3,
+    },
+    icon: {
+        color: colors.warmGrey,
+        fontSize: 20,
+    },
+})
+
+export default styles
