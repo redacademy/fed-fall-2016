9a91749be4ceed216a8f911c05c6b2f0a3897167
scottclayton <i@scottc.ca>
2016-12-12 15:31:44 -0800

    tidy MapPin component and add inline documentation

--- src/components/MapPin/index.js
+++ src/components/MapPin/index.js
@@ -8,31 +8,32 @@ import { mapPinColorChooser, babyFocusIconChooser } from '../../config/functions
 /** 
  * MapPin
  * Usage: 
- * 
- * <MapPin scale="0.5" pinColor={colors.apricot} iconName="add" />
- *
- * to get red icons… or replace with whatever color you want
- * <MapPin scale="0.5" pinColor={colors.apricot} iconName="add" iconColor="red" /> 
+<MapPin
+    scale="0.5"
+    amenities={put your amenities for the respective location here, which you're probably getting anyway when you pass in lng and lat}
+    />
+
  * 
  */
 class MapPin extends Component {
     render() {
+        const { amenities, scale } = this.props
         return (
             <View>
                 <Svg
                     style={{ alignItems: 'center', justifyContent: 'center' }}
-                    width={this.props.scale * 118.51}
-                    height={this.props.scale * 152.3}
+                    width={scale * 118.51}
+                    height={scale * 152.3}
                     >
                     <Path
                         d="M59.252,152.066c0,0,59.024-30.214,59.024-91.648c0-26.186-20.853-60.185-59.024-60.185	c-37.495,0-59.018,33.999-59.018,60.185C0.234,121.519,59.252,152.066,59.252,152.066z"
-                        fill={mapPinColorChooser(this.props.amenities)}
-                        scale={this.props.scale}
+                        fill={mapPinColorChooser(amenities)}
+                        scale={scale}
                         />
-                    <View style={{ top: -4 * this.props.scale }}>
+                    <View style={{ top: -4 * scale }}>
                         <Icon
-                            name={babyFocusIconChooser(this.props.amenities)}
-                            size={90 * this.props.scale}
+                            name={babyFocusIconChooser(amenities)}
+                            size={90 * scale}
                             color={colors.white}
                             />
                     </View>
