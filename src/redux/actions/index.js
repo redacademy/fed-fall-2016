8b318ac1f8309dbd614c35ed5c873dd43c92abb7
Tracey <traceywindsor@gmail.com>
2016-12-12 19:36:39 -0800

    change redux to handle action and move card

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -18,76 +18,81 @@ export const ON_SEARCH_CHANGE = 'ON_SEARCH_CHANGE'
 
 // Action creators here
 export const searchTextChange = (text) => ({
-  type: ON_SEARCH_CHANGE,
-  payload: text,
+    type: ON_SEARCH_CHANGE,
+    payload: text,
 })
 
 export const enterPreview = (placeId) => ({
-  type: ENTER_PREVIEW,
-  payload: placeId,
+    type: ENTER_PREVIEW,
+    payload: placeId,
 })
 
 export const exitPreview = () => ({
-  type: EXIT_PREVIEW,
+    type: EXIT_PREVIEW,
 })
 
 export const setCardPosition = (position) => {
-  switch (position) {
-    case 'full':
-      return { type: CARD_TO_POSITION_FULL }
-    case 'half':
-      return { type: CARD_TO_POSITION_HALF }
-    case 'directions':
-      return { type: CARD_TO_POSITION_DIRECTIONS }
-    case 'hidden':
-      return { type: CARD_TO_POSITION_HIDDEN }
-    default:
-      return null
-  }
+    switch (position) {
+        case 'full':
+            return { type: CARD_TO_POSITION_FULL }
+        case 'half':
+            return { type: CARD_TO_POSITION_HALF }
+        case 'directions':
+            return { type: CARD_TO_POSITION_DIRECTIONS }
+        case 'hidden':
+            return { type: CARD_TO_POSITION_HIDDEN }
+        default:
+            return null
+    }
 }
 
-export const setSelectedCard = (card, placeId, locationList) => {
-  switch (card) {
-    case 'LocationAdd':
-      return { type: LOCATION_ADD_LOAD, payload: placeId }
-    case 'LocationFilter':
-      return { type: LOCATION_FILTER_LOAD }
-    case 'LocationList':
-      return { type: LOCATION_LIST_LOAD, payload: locationList }
-    case 'LocationPreview':
-      return { type: LOCATION_VIEW_LOAD, payload: placeId }
-    case 'LocationRate':
-      return { type: LOCATION_RATE_LOAD, payload: placeId }
-    // case 'UserLocationList':
-    //   return { type: USER_LOCATION_LIST_LOAD, payload: locationList }
-    default:
-      return null
-  }
+export const setSelectedCard = (card, placeId, locationList) => dispatch => {
+    switch (card) {
+        case 'LocationAdd':
+            dispatch({ type: LOCATION_ADD_LOAD, payload: placeId })
+            return dispatch(setCardPosition('full'))
+        case 'LocationFilter':
+            dispatch({ type: LOCATION_FILTER_LOAD })
+            return dispatch(setCardPosition('full'))
+        case 'LocationList':
+            dispatch({ type: LOCATION_LIST_LOAD, payload: locationList })
+            return dispatch(setCardPosition('full'))
+        case 'LocationPreview':
+            dispatch({ type: LOCATION_VIEW_LOAD, payload: placeId })
+            return dispatch(setCardPosition('half'))
+        case 'LocationRate':
+            dispatch({ type: LOCATION_RATE_LOAD, payload: placeId })
+            return dispatch(setCardPosition('full'))
+        // case 'UserLocationList':
+        //   return { type: USER_LOCATION_LIST_LOAD, payload: locationList }
+        default:
+            return null
+    }
 }
 
 // Thunks down here
 export const generateMapPins = () => {
-  return function (dispatch) {
-    fetch('http://45.55.2.200/api/location', {
-      method: 'GET',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-      }),
-    })
-      .then(response => response.json())
-      .then(mapData => dispatch({ type: 'LOCATION_DATA_ALL', payload: mapData }))
-  }
+    return function (dispatch) {
+        fetch('http://45.55.2.200/api/location', {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+        })
+            .then(response => response.json())
+            .then(mapData => dispatch({ type: 'LOCATION_DATA_ALL', payload: mapData }))
+    }
 }
 
 export const getLocationDetails = (placeId) => {
-  return function (dispatch) {
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?place_id=${placeId}&key=AIzaSyB2WkbsqNDjsiz8i831IVn1piVIq5OeiCI`, {
-      method: 'GET',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-      }),
-    })
-      .then(response => response.json())
-      .then(locationDetails => dispatch({ type: 'LOCATION_DATA_DETAILS', payload: locationDetails }))
-  }
+    return function (dispatch) {
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?place_id=${placeId}&key=AIzaSyB2WkbsqNDjsiz8i831IVn1piVIq5OeiCI`, {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+        })
+            .then(response => response.json())
+            .then(locationDetails => dispatch({ type: 'LOCATION_DATA_DETAILS', payload: locationDetails }))
+    }
 }
