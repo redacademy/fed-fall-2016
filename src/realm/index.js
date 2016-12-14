bcf71e1ed967889ab9ff33d53a0faeaff29cb074
Tracey <traceywindsor@gmail.com>
2016-12-13 16:35:08 -0800

    m

--- src/realm/index.js
+++ src/realm/index.js
@@ -1,6 +1,19 @@
+<<<<<<< 1f424e44d7dd4418ad354c3b502cc2619162a3ee
 import Realm from 'realm'
 import Location from './location/schema'
 
 // Initialize a Realm with Location models
 let realm = new Realm({ schema: [Location] })
+=======
+const Realm = require('realm');
+
+let realm = new Realm({
+    schema: [{ name: '', properties: { name: 'string' } }]
+});
+
+realm.write(() => {
+    realm.create('', { name: '' });
+});
+
+>>>>>>> m
 
