73f4a91d5be2cfc6692783325168d38b53ddf8a5
Tracey <traceywindsor@gmail.com>
2016-12-14 21:43:26 -0800

    add __REDUX_DEVTOOLS_EXTENSION_COMPOSE__

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,11 +6,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const initialState = {}
 
-const store = createStore(rootReducer, initialState, composeEnhancers(
-    applyMiddleware(thunkMiddleware)
-))
-
-
-
+const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 export default store
