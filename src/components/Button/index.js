c872ff34ee4da46179b839185b502246455cf90c
Tracey <traceywindsor@gmail.com>
2016-12-14 21:45:25 -0800

    update Button to take optional OnPress function

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 import { TouchableOpacity } from 'react-native'
 import styles from './styles'
 
 import { colors } from '../../config/styles'
 
-const Button = ({ children, onPressFn }) => {
-    return (
-        <TouchableOpacity underlayColor={colors.lightGreyGreenTwo} style={[styles.button]} onPress={() => {}}>
-            {children}
-        </TouchableOpacity>
-    )
+class Button extends Component {
+    static propTypes = {
+        onPress: PropTypes.func,
+    }
+    constructor(props) {
+        super(props)
+        this._handlePress = this._handlePress.bind(this)
+    }
+    _handlePress() {
+        if (this.props.onPress)
+            this.props.onPress()
+    }
+    render() {
+        return (
+            <TouchableOpacity underlayColor={colors.lightGreyGreenTwo} style={[styles.button]} onPress={() => this._handlePress()}>
+                {this.props.children}
+            </TouchableOpacity>
+        )
+    }
 }
 
 export default Button
